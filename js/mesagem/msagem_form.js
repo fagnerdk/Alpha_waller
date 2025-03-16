2997b717283253aeb4d075082a560b60c74cb864
fagner <100000573+fagnerdk@users.noreply.github.com>
2025-03-15 23:27:41 -0300

    Alpha_0.003

--- js/mesagem/msagem_form.js
+++ js/mesagem/msagem_form.js
@@ -4,41 +4,55 @@
  let usuario = document.querySelector(".usuario");
  let mage_top =400
 
+ socket.on('hello', (arg) => { 
+let usuario_locau = document.createElement('li') 
+usuario_locau.style.backgroundColor =" rgb(5, 134, 246)"
+usuario_locau.style.padding = "5px";
+usuario_locau.style.width = "100px";
+usuario_locau.style.marginLeft = `20px`;
+usuario_locau.style.marginTop = `${mage_top}`;
+usuario_locau.style.borderRadius = "80px 80px 0px 80px";
+usuario_locau.style.display='flex'
+usuario_locau.style.textAlign='center'
+usuario_locau.style.margin='10px'
+usuario_locau.innerHTML = arg
+document.getElementById("lista").appendChild(usuario_locau);
+})
+
+socket.on('message',(text) =>{
 
 
-socket.on('message',text =>{
 
  let usuario = document.createElement('li')
  
-        usuario.id = socket.id
-        console.log(usuario)
-        usuario.style.backgroundColor = "rgb(5, 134, 246)";
+        usuario.style.backgroundColor = "rgb(5, 246, 9)";
         usuario.style.padding = "5px";
         usuario.style.width = "100px";
-        usuario.style.marginLeft = `20px`;
+        usuario.style.marginLeft = `190px`;
         usuario.style.marginTop = `${mage_top}`;
-        usuario.style.borderRadius = "80px 80px 0px 80px";
-        usuario.style.display='flex'
-        usuario.style.textAlign='center'
-        usuario.style.margin='10px'
-        usuario.innerHTML = text
+        usuario.style.margin='15px;'
+        usuario.style.borderRadius = "80px 80px 80px 0px";
+        usuario.style.textAlign="center"
+        usuario.innerHTML=text
         document.getElementById("lista").appendChild(usuario);
 
     inp.value=''
 })
 
-function enviar_for(id_form_msg) {
- 
+
+function enviar_for() {
+
 const text=inp.value
 
 if (inp.value=='') {
   console.log('erro')
+   inp.value=''
 }else{
  socket.emit('message',text)
+   inp.value=''
 
  }
  
-    
   //console.log(inp.value)
  
 }
